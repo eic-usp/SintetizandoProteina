0bae505cfdb5f747e439ff7c281909b10ecfb497
Lucas Xavier Leite <lucasxavierleite@pm.me>
2023-03-24 17:18:01 -0300

    Update game over panel

--- Assets/Game/Scripts/UI/Menu/GameOverMenu.cs
+++ Assets/Game/Scripts/UI/Menu/GameOverMenu.cs
@@ -4,7 +4,7 @@ namespace UI.Menu
 {
     public class GameOverMenu : MonoBehaviour
     {
-        public void RestartLevel() => GameSceneManagement.Loader.Load(GameSceneManagement.Loader.Scene.Gameplay);
-        public void Quit() => GameSceneManagement.Loader.Load(GameSceneManagement.Loader.Scene.UIBeg);
+        public void BackToMenu() => GameSceneManagement.Loader.Load(GameSceneManagement.Loader.Scene.UIBeg);
+        public void Quit() => Application.Quit();
     }
 }
